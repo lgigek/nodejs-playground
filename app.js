fa5722fb49f2a1b546fb12ca2d1d8c422605bf09
lgigek <lucasgigek@gmail.com>
2019-04-03 19:29:14 -0300

    configured app to use route and connect do db

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+const express = require('express');
+const post_route = require('./routes/post');
+const mongoose = require('./dbs/mongoose');
+
+// creates app
+const app = express();
+
+// adds post's router in app
+app.use('/api/post', post_route);
+
+// connects to mongoose
+mongoose.connect();
+
+// sets app port
+const port = process.env.APP_PORT;
+app.listen(port, () => console.log(`App running on port ${port}`));
