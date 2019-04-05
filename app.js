38d0deb9397e4a6f835c39bfee8ab870249a1ba0
lgigek <lucasgigek@gmail.com>
2019-04-05 15:15:18 -0300

    changed folder structure

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const post_route = require('./routes/post');
-const mongoose = require('./dbs/mongoose');
+const post_route = require('./app/routes/post');
+const mongodb = require('./app/dbs/mongoose');
 
 // creates app
 const app = express();
@@ -11,8 +11,8 @@ app.use(express.json());
 // adds post's router in app
 app.use('/api/post', post_route);
 
-// connects to mongoose
-mongoose.connect();
+// connects to mongodb
+mongodb.connect();
 
 // sets app port
 const port = process.env.APP_PORT;
