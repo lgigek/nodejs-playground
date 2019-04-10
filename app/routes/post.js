9f3ad7fadf6d6d19ca8c21aff687f753fc1056af
lgigek <lucasgigek@gmail.com>
2019-04-10 18:24:41 -0300

    created delete route

--- app/routes/post.js
+++ app/routes/post.js
@@ -51,4 +51,26 @@ router.post('/', async (req, res) => {
 
 });
 
+// route that deletes a post
+router.delete('/:id', async (req, res) => {
+    let id = req.params.id;
+    console.log(`HTTP Request do delete a post with id: ${id}`);
+
+    // verifies if id is valid
+    if (!service.is_id_valid(id)) {
+        console.log('Invalid id, returning 400');
+        return res.status(400).send({message: 'Invalid id'})
+    }
+
+    let result = await service.delete_by_id(id);
+    // verifies if post exists
+    if (!result) {
+        console.log('Post not found, returning 404');
+        return res.status(404).send({message: 'Post not found'})
+    } else {
+        console.log('Post successfully deleted');
+        return res.status(200).send(result);
+    }
+});
+
 module.exports = router;
--- app/service/post.js
+++ app/service/post.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const Post = require('../models/post');
+const mongoose = require('mongoose');
 
 async function get_all_posts() {
     return await Post.find();
@@ -13,6 +14,10 @@ async function insert_post(object) {
     return await Post(object).save();
 }
 
+async function delete_by_id(id) {
+    return await Post.findByIdAndDelete({_id: id});
+}
+
 function is_a_post(post) {
     let schema = {
         title: Joi.string().required(),
@@ -23,8 +28,15 @@ function is_a_post(post) {
     return Joi.validate(post, schema);
 }
 
+function is_id_valid(id) {
+    return mongoose.Types.ObjectId.isValid(id);
+}
+
 module.exports = {
     get_all_posts,
     get_by_title,
-    insert_post, is_a_post
+    insert_post,
+    delete_by_id,
+    is_a_post,
+    is_id_valid
 };
